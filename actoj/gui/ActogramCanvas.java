05a72615c397d188798d40a14db273f3ccf755b6
Benjamin Schmid <bene.schmid@gmail.com>
2010-07-27 12:54:05 +0200

    Draw a calibration bar over at the top of an ActogramCanvas

--- actoj/gui/ActogramCanvas.java
+++ actoj/gui/ActogramCanvas.java
@@ -2,6 +2,7 @@ package actoj.gui;
 
 import actoj.core.*;
 
+import javax.swing.JPanel;
 import javax.swing.JComponent;
 
 import java.awt.Point;
@@ -24,32 +25,90 @@ import ij.process.ImageProcessor;
  * A JComponent representing one actogram plus accompanying data, like
  * calibration. It uses a ActogramProcessor to draw the actogram itself.
  */
-public class ActogramCanvas extends JComponent
+public class ActogramCanvas extends JPanel
 			implements MouseMotionListener, MouseListener {
 	
+	/** The ActogramProcessor for drawing the actogram itself. */
 	public final ActogramProcessor processor;
 
+	/** The width of this component. */
 	public final int width;
+
+	/** The height of this component. */
 	public final int height;
 
+	/**
+	 * The Feedback which is called when the freerunning period triangle
+	 * has changed.
+	 */
 	private final Feedback feedback;
 
-	public ActogramCanvas(Actogram actogram, int zoom, float uLimit, int ppl, Feedback f) {
+	/**
+	 * Start point of the freerunning period triangle within the
+	 * ActogramProcessor coordinate system.
+	 */
+	private Point start = null;
+
+	/**
+	 * Current(end) point of the freerunning period triangle within the
+	 * ActogramProcessor coordinate system.
+	 */
+	private Point curr = null;
+
+	/** Distance between left border and the actogram. */
+	private int INT_LEFT = 5;
+
+	/** Distance between right border and the actogram. */
+	private int INT_RIGHT = 5;
+
+	/** Distance between top border and the actogram. */
+	private int INT_TOP = 20;
+
+	/** Distance between bottom border and the actogram. */
+	private int INT_BOTTOM = 5;
+
+	/** Stroke for the freerunning period triangle */
+	private final BasicStroke stroke = new BasicStroke(3f);
+
+	/** Font for the freerunning period triangle */
+	private final Font font = new Font("Helvetica", Font.BOLD, 14);
+
+	/** Text color for the freerunning period triangle */
+	private final Color text = new Color(149, 255, 139);
+
+	/** Color for the freerunning period triangle */
+	private final Color color = new Color(26, 93, 0);
+
+	/** Background color for the canvas */
+	private final Color background = new Color(139, 142, 255);
+
+
+	public ActogramCanvas(
+				Actogram actogram,
+				int zoom,
+				float uLimit,
+				int ppl,
+				Feedback f) {
 		super();
 		this.processor = new ActogramProcessor(actogram, zoom, uLimit, ppl);
 		this.feedback = f;
 
 		ImageProcessor ip = processor.processor;
-		width = ip.getWidth();
-		height = ip.getHeight();
+		width = ip.getWidth() + INT_LEFT + INT_RIGHT;
+		height = ip.getHeight() + INT_TOP + INT_BOTTOM;
 		this.setPreferredSize(new Dimension(width, height));
 
 		addMouseListener(this);
 		addMouseMotionListener(this);
+
+		setBackground(background);
 	}
 
+	// x and y are coordinates within this component
 	public String getPositionString(int x, int y) {
-		int idx = processor.getIndex(x, y);
+		if(x < INT_LEFT || y < INT_TOP || x >= width - INT_RIGHT || y >= height - INT_BOTTOM)
+			return "outside";
+		int idx = processor.getIndex(x - INT_LEFT, y - INT_TOP);
 		if(idx < 0)
 			return "outside";
 		Actogram a = processor.downsampled;
@@ -77,14 +136,11 @@ public class ActogramCanvas extends JComponent
 			tv.intervalIn(processor.downsampled.unit));
 	}
 
-	private final BasicStroke stroke = new BasicStroke(3f);
-	private final Font font = new Font("Helvetica", Font.BOLD, 14);
-	private final Color text = new Color(149, 255, 139);
-	private final Color color = new Color(26, 93, 0);
-
 	public void paintComponent(Graphics g) {
 		ImageProcessor ip = processor.processor;
-		g.drawImage(ip.createImage(), 0, 0, null);
+		g.setColor(background);
+		g.fillRect(0, 0, width, height);
+		g.drawImage(ip.createImage(), INT_LEFT, INT_TOP, null);
 
 
 		if(start != null) {
@@ -95,12 +151,15 @@ public class ActogramCanvas extends JComponent
 			int cIdx = processor.getIndex(cu.x, cu.y);
 			if(cIdx < 0) return;
 
+			Point s = new Point(st.x + INT_LEFT, st.y + INT_TOP);
+			Point c = new Point(cu.x + INT_LEFT, cu.y + INT_TOP);
+
 			Graphics2D g2d = (Graphics2D)g;
 			g2d.setColor(color);
 			g2d.setStroke(stroke);
-			g2d.drawLine(st.x, st.y, cu.x,  cu.y);
-			g2d.drawLine(st.x, st.y, st.x, cu.y);
-			g2d.drawLine(st.x, cu.y,  cu.x,  cu.y);
+			g2d.drawLine(s.x, s.y, c.x, c.y);
+			g2d.drawLine(s.x, s.y, s.x, c.y);
+			g2d.drawLine(s.x, c.y, c.x, c.y);
 			int dy = (cu.y - st.y) / processor.baselineDist; // in periods
 			int dx = Math.abs(cu.x - st.x);
 			dx *= processor.downsampled.interval.millis;
@@ -112,8 +171,8 @@ public class ActogramCanvas extends JComponent
 
 			// v string
 			Rectangle vr = new Rectangle(
-				st.x - fm.stringWidth(v) - 2,
-				(st.y + cu.y + fh) / 2,
+				s.x - fm.stringWidth(v) - 2,
+				(s.y + c.y + fh) / 2,
 				fm.stringWidth(v), fh);
 			g2d.setColor(color);
 			g2d.fillRect(vr.x, vr.y - fh + 2, vr.width, vr.height);
@@ -122,20 +181,38 @@ public class ActogramCanvas extends JComponent
 
 			// h string
 			Rectangle hr = new Rectangle(
-				(st.x + cu.x - fm.stringWidth(h)) / 2,
-				cu.y + fh + 2,
+				(s.x + c.x - fm.stringWidth(h)) / 2,
+				c.y + fh + 2,
 				fm.stringWidth(h), fh);
 			g2d.setColor(color);
 			g2d.fillRect(hr.x, hr.y - fh + 2, hr.width, hr.height);
 			g2d.setColor(text);
 			g2d.drawString(h, hr.x, hr.y);
 		}
+
+		drawCalibration(g, 8);
+	}
+
+	private void drawCalibration(Graphics gr, int subd) {
+		int w = width - INT_LEFT - INT_RIGHT;
+
+		Graphics2D g = (Graphics2D)gr;
+		g.setColor(Color.BLACK);
+		g.setStroke(new BasicStroke(1f));
+		g.drawLine(INT_LEFT, 10, width - INT_RIGHT, 10);
+		for(int i = 0; i <= subd; i++) {
+			int x = INT_LEFT + (int)Math.round((float)i * w / subd);
+			g.drawLine(x, 10 - 5, x, 10 + 5);
+		}
 	}
 
 	public Point snap(Point in) {
+		if(in.x < INT_LEFT || in.y < INT_TOP ||
+			in.x >= width - INT_RIGHT || in.y >= height - INT_BOTTOM)
+			return null;
 		int bd = processor.baselineDist;
-		int idx = in.y / bd;
-		return new Point(in.x, (idx + 1) * bd);
+		int idx = (in.y - INT_TOP)/ bd;
+		return new Point(in.x - INT_LEFT, (idx + 1) * bd);
 	}
 
 	private Point upper() {
@@ -149,9 +226,6 @@ public class ActogramCanvas extends JComponent
 			return null;
 		return start.y <= curr.y ? curr : start;
 	}
-
-	private Point start = null;
-	private Point curr = null;
 
 	public void mouseEntered(MouseEvent e) {}
 	public void mouseExited(MouseEvent e) {}
@@ -179,8 +253,6 @@ public class ActogramCanvas extends JComponent
 		if(feedback != null)
 			feedback.positionChanged(
 				getPositionString(e.getX(), e.getY()));
-// 		if(e.getButton() != MouseEvent.BUTTON1)
-// 			return;
 		Point tmp = snap(e.getPoint());
 		if(!tmp.equals(curr)) {
 			curr = tmp;
